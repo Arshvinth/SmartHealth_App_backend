4e5f3c66cd35ed515adb47afc483906e0d72d5b9
Poojani K.H.S <sahanyapoojani.k.h@gmail.com>
2025-10-15 19:30:50 +0530

    all commits

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -7,36 +8,34 @@ import connectDB from './config/mongodb.js';
 import bodyParser from 'body-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { auditMiddleware } from './middlewares/auditLogger.js';
-import patientRoute from './routes/patientRoute.js'
+import patientRoute from './routes/patientRoute.js';
 
-
-// import Routes
-/*import routerName from './PATH';*/
-
-//app config
 const app = express();
+
+// Middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('combined'));
-
-//app.use(express.json());
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8081
-connectDB()
-
-// Api endpoints
+// Routes
 app.use(auditMiddleware);
 app.use('/api/patients', patientRoute);
-// global error handler
 app.use(errorHandler);
 
-app.get('/',(req,res)=>{
-  res.send('API Working')
-})
-
-// Start server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${port}`);
+app.get('/', (req, res) => {
+  res.send('API Working');
 });
+
+// Only start server if not running under Jest
+if (process.env.NODE_ENV !== 'test') {
+  const port = process.env.PORT || 8081;
+  connectDB();
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+  });
+}
+
+// ✅ Export app for testing
+export default app;
